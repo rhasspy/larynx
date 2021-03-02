c9123f76e30f37b9a6df9dd832e152b0bb697524
Michael Hansen <michael.hansen.24@us.af.mil>
2021-03-02 13:42:52 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,49 @@
+"""Setup file for larynx runtime"""
+import os
+from pathlib import Path
+
+import setuptools
+
+this_dir = Path(__file__).parent
+
+# -----------------------------------------------------------------------------
+
+# Load README in as long description
+long_description: str = ""
+readme_path = this_dir / "README.md"
+if readme_path.is_file():
+    long_description = readme_path.read_text()
+
+requirements = []
+requirements_path = this_dir / "requirements.txt"
+if requirements_path.is_file():
+    with open(requirements_path, "r") as requirements_file:
+        requirements = requirements_file.read().splitlines()
+
+version_path = this_dir / "VERSION"
+with open(version_path, "r") as version_file:
+    version = version_file.read().strip()
+
+# -----------------------------------------------------------------------------
+
+setuptools.setup(
+    name="larynx_runtime",
+    version=version,
+    description="End-to-end text to speech using gruut and onnx models",
+    author="Michael Hansen",
+    author_email="mike@rhasspy.org",
+    url="https://github.com/rhasspy/larynx",
+    packages=setuptools.find_packages(),
+    package_data={"larynx_runtime": ["py.typed"]},
+    install_requires=requirements,
+    entry_points={"console_scripts": ["larynx_runtime = larynx_runtime.__main__:main"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.7",
+)
